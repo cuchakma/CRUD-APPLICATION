be83be92afa0657dbcfbc9be7ef22b74759a1f28
cuchakma <greendaycu20@gmail.com>
2020-10-28 19:03:45 +0600

    updated upto 28.10.2020

---
+++ inc/templates/nav.php
@@ -0,0 +1,6 @@
+<p>
+    <a href="/project%20exercises/Projects/CRUD/?task=report">All STUDENTS&nbsp&nbsp;</a>
+    <a href="/project%20exercises/Projects/CRUD/?task=add">ADD NEW STUDENTS&nbsp&nbsp;</a>
+    <a href="/project%20exercises/Projects/CRUD/?task=seed">SEED&nbsp&nbsp;</a>
+</p>
+
